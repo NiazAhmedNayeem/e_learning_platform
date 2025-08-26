accd1871e0b0314b5282ad271cfef64cfe621d05
NiazAhmedNayeem <niazahmed.net@gmail.com>
2025-08-26 15:53:30 +0600

    Update some code

--- resources/views/backend/layouts/partials/script.blade.php
+++ resources/views/backend/layouts/partials/script.blade.php
@@ -18,6 +18,9 @@
 {{-- summernote --}}
 <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.js"></script>
 
+<!-- Bootstrap Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+
 <script>
     $(document).ready(function() {
         // toastr messages
